2fb1e5effd60ead06b22f06b7f9c424a5bb4415f
Eronn118 <130540871+Eronn118@users.noreply.github.com>
2024-11-14 20:36:49 +0800

    Profile
    medyo ok na yung responsiveness

--- src/profile.jsx
+++ src/profile.jsx
@@ -8,21 +8,25 @@ import dashboardIcon from './icon/dashboard.png';
 import settingsIcon from './icon/settings.png';
 import helpIcon from './icon/help.png';
 
+// Define SidebarItem component
 const SidebarItem = ({ icon, text, link }) => (
     <li>
-        <img src={icon} alt={`${text} Icon`} width="30%" height="30%" />
-        <a href={link}>{text}</a>
+        <a href={link}>
+            <img src={icon} alt={text} />
+            {text}
+        </a>
     </li>
 );
 
-
-const Profile = () => {
+const Profile = () => { 
     return (
         <div>
             <header className="header">
                 <h1>Logo here</h1>
-                <img src={nameIcon} alt="Profile" />
-                <img src={notifIcon} alt="Notifications" />
+                <div className="header-icons">
+                    <img src={nameIcon} alt="Profile" />
+                    <img src={notifIcon} alt="Notifications" />
+                </div>
             </header>
 
             <nav className="sidebar">
@@ -34,39 +38,35 @@ const Profile = () => {
                     <SidebarItem icon={helpIcon} text="Help" link="help" />
                 </ul>
             </nav>
+
             <div className="profile-container">
-                <div className="settings-section">
-                    <div className="profilebox" style={{ flexGrow: 1 }}>
-                        <h2>Account Profile</h2>
-                        <div className="row">
-                            <div className="profile-image">
-                                <img src={nameIcon} alt="Profile Picture" width="100%" height="100%" />
-                            </div>
-                            <div></div>
-                        </div>
-                    </div>
-                    <div className="sectionholder">
-                        <div className="section-box">
-                            <div className="row">
-                                <label>Name</label>
-                            </div>
-                            <div className="row">
-                                <label>Age</label>
-                            </div>
-                            <div className="row">
-                                <label>Id Number</label>
-                            </div>
-                            <div className="row">
-                                <label>Program</label>
-                            </div>
-                        </div>
-                        <div className="section-box">
-                            <div className="row">
-                                <label>Hours Activity</label>
-                            </div>
-                        </div>
-                    </div>
+                <div className="header-container">
+                    <h2>Account Profile</h2>
+                    <div class="header-underline"></div>
                 </div>
+
+               <div className="profile-wrapper">
+    <div className="profilebox">
+        <div className="profile-image">
+            <img src={nameIcon} alt="Profile Picture" />
+        </div>
+        
+    </div>
+    <div className="separator-line"></div>
+    <div className="section-holder">
+        <div className="section personal-info">
+            <h3>Personal Information</h3>
+            <div className="row"><label>Name</label></div>
+            <div className="row"><label>Age</label></div>
+            <div className="row"><label>Id Number</label></div>
+            <div className="row"><label>Program</label></div>
+        </div>
+        <div className="section activity-info">
+            <h3>Activity Information</h3>
+            <div className="row"><label>Hours Activity</label></div>
+        </div>
+    </div>
+</div>
             </div>
         </div>
     );
