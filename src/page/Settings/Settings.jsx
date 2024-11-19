77fb4a6a46d68aedf8c9caf199b9b6222471c3f8
Mao07077 <106759453+Mao07077@users.noreply.github.com>
2024-11-19 14:43:09 +0800

    Revert "Merge branch 'main' of https://github.com/Mao07077/cbrcs"
    This reverts commit 79c4144c486ea51c8c6640a9af7f42e100320cad, reversing
    changes made to d0c31ce883689896e5183eb1e45f6666d211de25.

--- src/page/Settings/Settings.jsx
+++ src/page/Settings/Settings.jsx
@@ -1,111 +1,83 @@
 import React from 'react';
-import './settings.css';
+import './Settings.css';
 
-import nameIcon from './icon/name.png';
-import notifIcon from './icon/notif.png';
-import moduleIcon from './icon/module.png';
-import dashboardIcon from './icon/dashboard.png';
-import settingsIcon from './icon/settings.png';
-import helpIcon from './icon/help.png';
+import nameIcon from '../../icon/name.png';
+import notifIcon from '../../icon/notif.png';
+import moduleIcon from '../../icon/module.png';
+import dashboardIcon from '../../icon/dashboard.png';
+import settingsIcon from '../../icon/settings.png';
+import helpIcon from '../../icon/help.png';
 
 const SidebarItem = ({ icon, text, link }) => (
-  <li>
-    <img src={icon} alt={`${text} Icon`} width="30%" height="30%" />
-    <a href={link}>{text}</a>
-  </li>
+    <li>
+        <img src={icon} alt={`${text} Icon`} width="30%" height="30%" />
+        <a href={link}>{text}</a>
+    </li>
 );
 
+
 const Settings = () => {
-  return (
-    <div>
-      <header className="header">
-        <div className="header-content">
-          <div className="header-logo">
-            <img src={logoIcon} alt="logo" />
-          </div>
+    return (
+        <div>
+            <header className="header">
+                <h1>Logo here</h1>
+                <img src={nameIcon} alt="Profile" />
+                <img src={notifIcon} alt="Notifications" />
+            </header>
+            <nav className="sidebar">
+                <ul>
+                    <SidebarItem icon={nameIcon} text="Name" link="profile" />
+                    <SidebarItem icon={moduleIcon} text="Module" link="module" />
+                    <SidebarItem icon={dashboardIcon} text="Dashboard" link="dashboard" />
+                    <SidebarItem icon={settingsIcon} text="Settings" link="settings" />
+                    <SidebarItem icon={helpIcon} text="Help" link="help" />
+                </ul>
+            </nav>
+            <div className="settings-container">
+                <div className="settings-section">
+                    {/* General Section */}
+                    <div className="sec-box">
+                        <h2>General</h2>
+                        <div className="row">
+                            <div className="profile-image">
+                                {/* Removed icon */}
+                            </div>
+                            <div style={{ flexGrow: 1 }}>
+                                <div className="row">
+                                    <label>Full Name</label>
+                                    <input type="text" placeholder="Enter full name" />
+                                </div>
+                                <div className="row">
+                                    <label>Birthdate</label>
+                                    <input type="text" placeholder="Enter birthdate" />
+                                </div>
+                                <div className="row">
+                                    <label>Email</label>
+                                    <input type="text" placeholder="Enter email" />
+                                </div>
+                                <div className="row">
+                                    <label>Program</label>
+                                    <input type="text" placeholder="Enter program" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Login Info Section */}
+                    <div className="sec-box">
+                        <h2>Login Info</h2>
+                        <div className="row">
+                            <label>Username</label>
+                            <input type="text" placeholder="Enter username" />
+                        </div>
+                        <div className="row">
+                            <label>Password</label>
+                            <input type="password" placeholder="Enter password" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </header>
-      <nav className="sidebar">
-        <ul>
-          <li>
-            <img src={nameIcon} alt="Name icon" className="sidebar-icon" />
-            <Link to="/">Name</Link>
-          </li>
-          <li>
-            <img src={moduleIcon} alt="Module icon" className="sidebar-icon" />
-            <Link to="/module">Module</Link>
-          </li>
-          <li>
-            <img src={dashboardIcon} alt="dashboard icon" className="sidebar-icon" />
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <img src={settingsIcon} alt="Settings icon" className="sidebar-icon" />
-            <Link to="/settings">Settings</Link>
-          </li>
-          <li>
-            <img src={helpIcon} alt="Help icon" className="sidebar-icon" />
-            <Link to="/help">Help</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="settings-container">
-        <div className="settings-section">
-          {/* General Section */}
-          <div className="sec-box">
-            <h2>General</h2>
-            <div className="row">
-              <div className="profile-image">
-                {/* Removed icon */}
-              </div>
-              <div style={{ flexGrow: 1 }}>
-                <div className="row">
-                  <label>Full Name</label>
-                  <input type="text" placeholder="Enter full name" />
-                </div>
-                <div className="row">
-                  <label>Birthdate</label>
-                  <input type="text" placeholder="Enter birthdate" />
-                </div>
-                <div className="row">
-                  <label>Email</label>
-                  <input type="text" placeholder="Enter email" />
-                </div>
-                <div className="row">
-                  <label>Program</label>
-                  <input type="text" placeholder="Enter program" />
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Login Info Section */}
-          <div className="sec-box">
-            <h2>Login Info</h2>
-            <div className="row">
-              <label>Username</label>
-              <input type="text" placeholder="Enter username" />
-            </div>
-            <div className="row">
-              <label>Password</label>
-              <input type="password" placeholder="Enter password" />
-            </div>
-          </div>
-          {/* Settings Section (added content) */}
-          <div className="settings-section">
-            <div className="general-info">
-              {/* General Info Content */}
-            </div>
-            <div className="login-info">
-              {/* Login Info Content */}
-            </div>
-            <div className="submit-button-container">
-  <button type="submit" className="submit-button">Submit</button>
-</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Settings;
