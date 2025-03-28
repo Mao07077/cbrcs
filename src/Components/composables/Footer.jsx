8d1b9fc6d9f43ee6491bdecf62b35959c8e831d0
Lue Kely <luekely43110@gmail.com>
2025-03-28 09:45:24 +0800

    added social links

--- src/Components/composables/Footer.jsx
+++ src/Components/composables/Footer.jsx
@@ -1,22 +1,43 @@
 //
 import Styles from './Footer.module.css';
 import Icon from '../../icon/actual.png';
-
+import { Facebook, Globe } from 'lucide-react';
 /**
  * Footer component.
  * @returns {JSX.Element} The Footer component.
  * @description This is a resuable footer  import this to any parent component to apply the footer */
 
 export default function Footer() {
+	const footerInfo = {
+		address:
+			'4th Flr., MGSC Bldg., Alabang-Zapote Road, Almanza Uno, Las Piñas City',
+		phoneNo: '09664059257',
+		fb: 'https://www.facebook.com/CarlBalitaLP?rdid=gbKbc1KgF7wuSaNh&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F15wPaMJrxP%2F',
+		website: 'https://drcarlbalitareviewcenter.com/',
+	};
+
 	return (
 		<footer className={Styles.footer}>
 			{/* insert logo here */}
 			<ul>
-				<h2>Contact Us</h2>
-				<li>Phone No#: </li>
-				<li>Address:</li>
-				<li>Email:</li>
+				<h2>Contact Us </h2>
+				<li>Phone No#: {footerInfo.phoneNo} </li>
+				<li>Address: {footerInfo.address} </li>
+				{/* socials */}
+				<li>
+					Facebook:&nbsp;
+					<a href={footerInfo.fb} target="_blank">
+						Carl Balita
+					</a>
+				</li>
+				<li>
+					Website: &nbsp;
+					<a href={footerInfo.website} target="_blank">
+						drcarlbalitareviewcenter.com
+					</a>
+				</li>
 			</ul>
+
 			<div className={Styles.Logo_Wrapper}>
 				<img src={Icon} alt="logo" />
 				<p>© 2024 Dr. Carl Balita Review Center. All Rights Reserved.</p>
