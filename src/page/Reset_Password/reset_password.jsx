f938f6b5a6d6ab3556429f37b8c669c576623170
Mao07077 <106759453+Mao07077@users.noreply.github.com>
2025-04-01 15:06:10 +0800

    4

--- src/page/Reset_Password/reset_password.jsx
+++ src/page/Reset_Password/reset_password.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import MainHeader from '../../Components/MainHeader';
 import Styles from './Reset_password.module.css';
 
+// Set API URL dynamically based on the environment
 const API_URL = process.env.REACT_APP_API_URL || 
     (window.location.hostname === "localhost" ? "http://127.0.0.1:8000" : "https://cbrcs.onrender.com");
 
