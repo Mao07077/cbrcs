0552dc13a53ef58542d2c04dd440cb962e593c9c
Mao07077 <106759453+Mao07077@users.noreply.github.com>
2025-05-11 11:57:37 +0800

    sakit sa ulo

--- src/Instructor_Page/Create_module/Create_module.jsx
+++ src/Instructor_Page/Create_module/Create_module.jsx
@@ -18,7 +18,7 @@ const CreateModule = ({ onClose }) => {
 
   // Dynamically set the API_URL based on the environment
   const API_URL = process.env.REACT_APP_API_URL || 
-  (window.location.hostname === "localhost" ? "http://127.0.0.1:8000" : "https://14c1-2405-8d40-4479-50f0-25aa-3e85-9a34-71e6.ngrok-free.app ");
+  (window.location.hostname === "localhost" ? "http://127.0.0.1:8000" : "https://1945-2405-8d40-4479-50f0-25aa-3e85-9a34-71e6.ngrok-free");
 
 
   const handleSubmit = async (e) => {
