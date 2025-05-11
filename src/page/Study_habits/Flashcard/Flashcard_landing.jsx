22f38d713d1afd784e74ba450c62d7051d7908ae
Mao07077 <106759453+Mao07077@users.noreply.github.com>
2025-05-11 10:51:19 +0800

    wait

--- src/page/Study_habits/Flashcard/Flashcard_landing.jsx
+++ src/page/Study_habits/Flashcard/Flashcard_landing.jsx
@@ -15,7 +15,7 @@ const FlashcardsLandingPage = () => {
         process.env.REACT_APP_API_URL ||
         (window.location.hostname === 'localhost'
             ? 'http://localhost:3000'
-            : 'https://a7bc-2405-8d40-4479-50f0-25aa-3e85-9a34-71e6.ngrok-free.app');
+            : 'https://05d5-2405-8d40-4479-50f0-25aa-3e85-9a34-71e6.ngrok-free.app');
 
     useEffect(() => {
         const fetchUserProfile = async () => {
