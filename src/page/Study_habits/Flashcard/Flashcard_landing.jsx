c383bdacfd6352aac77203c77d2fbf0744847add
JazzNase <jazzmichaelnase@gmail.com>
2025-06-04 19:41:54 +0800

    --ayaw gumana ng old ngrok--, pero gumagana yung bagong ngrok (nacomment ko naman)

--- src/page/Study_habits/Flashcard/Flashcard_landing.jsx
+++ src/page/Study_habits/Flashcard/Flashcard_landing.jsx
@@ -15,9 +15,8 @@ const FlashcardsLandingPage = () => {
         process.env.REACT_APP_API_URL ||
         (window.location.hostname === 'localhost'
             ? 'http://127.0.0.1:8000'
-
             : 'https://dfbd-110-54-166-204.ngrok-free.app');
-            : 'https://c3a1-2405-8d40-448f-2d57-c4b-6820-175b-382a.ngrok-free.app ');
+            // : 'https://c3a1-2405-8d40-448f-2d57-c4b-6820-175b-382a.ngrok-free.app');
 
     const requestHeaders = {
         'ngrok-skip-browser-warning': 'true',
--- src/page/Study_habits/Scheduler/Scheduler.jsx
+++ src/page/Study_habits/Scheduler/Scheduler.jsx
@@ -34,7 +34,7 @@ const ScheduleTable = () => {
             ? 'http://127.0.0.1:8000'
 
             : 'https://dfbd-110-54-166-204.ngrok-free.app');
-            : 'https://c3a1-2405-8d40-448f-2d57-c4b-6820-175b-382a.ngrok-free.app ');
+            // : 'https://c3a1-2405-8d40-448f-2d57-c4b-6820-175b-382a.ngrok-free.app ');
 
     useEffect(() => {
         const userId = localStorage.getItem('userIdNumber');
