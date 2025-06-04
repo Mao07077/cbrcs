aa463d9647b1e1b57a718dc5c783ce7b517fc4dd
JazzNase <jazzmichaelnase@gmail.com>
2025-06-04 20:23:50 +0800

    adding vercel in CORS in main2

--- src/backend/main2.py
+++ src/backend/main2.py
@@ -30,7 +30,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://cbrcs.vercel.app",
+        "https://cbrcs-git-aaron-maos-projects-a7ae5dee.vercel.app",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
