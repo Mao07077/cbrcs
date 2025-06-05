9023146a730e0c2c2e1279f052836d60777d3c82
Mao07077 <106759453+Mao07077@users.noreply.github.com>
2025-06-05 18:13:55 +0800

    oked

--- src/page/Module/Module.jsx
+++ src/page/Module/Module.jsx
@@ -13,11 +13,11 @@ const ModuleDashboard1 = () => {
   const navigate = useNavigate();
 
   const API_URL =
-    localStorage.getItem('REACT_APP_API_URL') ||
+    localStorage.getItem('https://cbrcs-1.onrender.com') ||
     process.env.REACT_APP_API_URL ||
     (window.location.hostname === "localhost"
       ? "http://127.0.0.1:8000"
-      : "https://cbrcs-1.onrender.com ");
+      : " ");
   // Common headers for all fetch requests
   const requestHeaders = {
     'ngrok-skip-browser-warning': 'true', // Bypasses ngrok warning page
